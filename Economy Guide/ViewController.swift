7f62f4ea3ec0b48efaded3796dcd4aa493bad4e0
Kaue <kaueludovico@outlook.com>
2021-04-13 10:44:31 -0300

    Initial Commit

---
+++ Economy Guide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Economy Guide
+//
+//  Created by Kauê Camargo Ludovico on 13/04/21.
+//  Copyright © 2021 Big Rocket Digital Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Economy GuideTests/Economy_GuideTests.swift
@@ -0,0 +1,34 @@
+//
+//  Economy_GuideTests.swift
+//  Economy GuideTests
+//
+//  Created by Kauê Camargo Ludovico on 13/04/21.
+//  Copyright © 2021 Big Rocket Digital Solutions. All rights reserved.
+//
+
+import XCTest
+@testable import Economy_Guide
+
+class Economy_GuideTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
